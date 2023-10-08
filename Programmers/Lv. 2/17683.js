4b65da34cb64d22d172c315d20680d4572170dcb
kangmin01 <minheek99@gmail.com>
2023-10-08 19:48:12 +0900

    2023-10-08

--- Programmers/Lv. 2/17683.js
+++ Programmers/Lv. 2/17683.js
@@ -53,6 +53,35 @@ function solution(m, musicinfos) {
   return res.length === 0 ? "(None)" : res[0];
 }
 
+// (1) : #을 소문자로 변환하여 풀이
+// const solution = (m, musicInfos) => {
+//   let answer = "";
+
+//   musicInfos = musicInfos.map((e) => {
+//     let eArr = e.split(",");
+//     let timeDiff =
+//       (new Date(`1970-01-01 ${eArr[1]}:00`) -
+//         new Date(`1970-01-01 ${eArr[0]}:00`)) /
+//       60000;
+//     let melody = eArr[3].replace(/[A-Z]#/g, (m) => m[0].toLowerCase());
+//     melody = melody
+//       .repeat(Math.ceil(timeDiff / melody.length))
+//       .substr(0, timeDiff);
+//     return `${timeDiff},${eArr[2]},${melody}`;
+//   });
+
+//   musicInfos.sort((a, b) => b.split(",")[0] - a.split(",")[0]);
+
+//   answer = musicInfos.filter(
+//     (e) =>
+//       e
+//         .split(",")[2]
+//         .indexOf(m.replace(/[A-Z]#/g, (m) => m[0].toLowerCase())) != -1
+//   );
+
+//   return answer.length == 0 ? "(None)" : answer[0].split(",")[1];
+// };
+
 console.log(
   solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])
 );
